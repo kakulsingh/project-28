b412a5cef4783ee45a07f363d17b377884b3fd0d
kakulsingh <73896965+kakulsingh@users.noreply.github.com>
2021-02-21 10:46:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5;
+var world,boy;
+var slingshot;
+function preload(){
+	boy=loadImage("images/boy.png");
+	stoneObj=loadImage("images/stone.png");
+
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	
+	Engine.run(engine);
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1150,150,30);
+	mango3=new mango(950,130,30);
+	mango4=new mango(1205,120,30);
+	mango5=new mango(1000,160,30);
+	stoneObj=new stone(230,420,20)
+	slingshot=new slingShot(stone.body,{x:200,y:100})
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+stoneObj.display();
+  groundObject.display();
+slingshot.display();
+}
+function mouseDragged(){
+	Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+	slingshot.fly()
+}
